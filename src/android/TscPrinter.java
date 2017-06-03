a56f7ab8a2ab1abca0f7c59ca6683b0f663d37f4
Anil Bhanushali <b.anil@better-life.co.in>
2017-06-03 19:54:39 +0530

    added test methods

--- src/android/TscPrinter.java
+++ src/android/TscPrinter.java
@@ -1,32 +1,87 @@
-package cordova-plugin-tsc-printer;
+package com.betterlife.tscprinter;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.example.tscdll.TSCActivity;
+
+import java.lang.reflect.Array;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class TscPrinter extends CordovaPlugin {
 
+    private static final String TAG = "TSCPlugin";
+    TSCActivity TscDll = new TSCActivity();
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Log.d(TAG, "Initializing TSCPlugin");
+    }
+
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("printBarCode")) {
+            try {
+                callbackContext.success(this.printBarCode(args));
+            }catch (Error err){
+                callbackContext.error(err.getMessage());
+            }
+            return true;
+        }else if(action.equals("connectPrinter")){
+            String macAddress = args.getString(0);
+            try {
+                callbackContext.success(this.connectPrinter(macAddress));
+            }catch (Error err){
+                callbackContext.error(err.getMessage());
+            }
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+
+    private String connectPrinter(String macAddress){
+        return TscDll.openport(macAddress);
     }
+
+    private String setUpPrinter(Integer width,Integer height,Integer speed, Integer density,Integer sensor,Integer sensor_distance,Integer sensor_offset){
+        return TscDll.setup(width,height,speed,density,sensor,sensor_distance,sensor_offset);
+    }
+
+    private String printBarCode(JSONArray args) throws JSONException {
+        Integer x = args.getInt(0);
+        Integer y = args.getInt(1);
+        String type = args.getString(2);
+        Integer height = args.getInt(3);
+        Integer human_readable = args.getInt(4);
+        Integer rotation = args.getInt(5);
+        Integer narrow = args.getInt(6);
+        Integer wide = args.getInt(7);
+        String content = args.getString(8);
+        return TscDll.barcode(x,y,type,height,human_readable,rotation,narrow,wide,content);
+    }
+
+    private String printLabel(Integer quantity,Integer copy){
+        return TscDll.printlabel(quantity, copy);
+    }
+
+    private String Disconnect(){
+        return TscDll.closeport();
+    }
+
+    private String getStatus(){
+        return TscDll.status();
+    }
+
 }
--- www/TscPrinter.js
+++ www/TscPrinter.js
@@ -8,39 +8,18 @@ exports.connectPrinter = function (address, successCallback, errorCallback) {
     cordova.exec(
         successCallback, // success callback function
         errorCallback, // error callback function
-        'Printer', // mapped to our native Java class called "Printer"
-        'connect', // with this action name
-        [{                  // and this array of custom arguments to create our entry
-            "macaddress": address
-        }]
+        'TscPrinter',
+        'connectPrinter', // with this action name
+        [address]
     );
 };
 
-exports.printText = function (macaddress, text, alignment, attribute, textsize, successCallback, errorCallback) {
+exports.printBarCode = function (args, successCallback, errorCallback) {
     cordova.exec(
         successCallback, // success callback function
         errorCallback, // error callback function
-        'Printer', // mapped to our native Java class called "Printer"
-        'printtext', // with this action name
-        [{
-            "macaddress": macaddress,// and this array of custom arguments to create our entry
-            "text": text,
-            "alignment": alignment,
-            "attribute": attribute,
-            "textsize": textsize
-        }]
+        'TscPrinter',
+        'printBarCode',
+        args
     );
 };
-
-exports.PrintImage = function (macaddress, file, successCallback, errorCallback) {
-    cordova.exec(
-        successCallback, // success callback function
-        errorCallback, // error callback function
-        'Printer', // mapped to our native Java class called "Printer"
-        'printimage', // with this action name
-        [{
-            "macaddress": macaddress,// and this array of custom arguments to create our entry
-            "file": file
-        }]
-    );
-}
